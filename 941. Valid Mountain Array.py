e51207ac33dbee0dbd6fb359aa9b36830dfcd1b6
Daniel Demerw <danieldemerw777@gmail.com>
2023-12-01 01:47:52 -0500

    941. Valid Mountain Array.py
    https://leetcode.com/problems/valid-mountain-array/description/

---
+++ 941. Valid Mountain Array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        if len(arr)<3:
+            return False
+        i =1
+        while i<len(arr) and arr[i]>arr[i-1]:
+            i+=1
+
+        if i==1 or i==len(arr):
+            return False
+        while i <len(arr) and arr[i]<arr[i-1]:
+            i+=1
+        return i==len(arr)
